76111b99dfa17e6b7e915544987dc12db412b5e0
Tayler Uva <tayleruva@gmail.com>
2019-02-20 15:52:31 -0800

    started hw

--- ShareRecipes/MainPage.xaml.cs
+++ ShareRecipes/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,19 @@ namespace ShareRecipes {
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
+            PopulateListView();
+        }
+
+        private void PopulateListView() {
+            var pokedex = new ObservableCollection<Pokemon>();
+
+            var dex001 = new Pokemon {
+                Name = "Bulbasuar",
+                DexNum = "001",
+                RoutesSeen = "None"
+            };
+            pokedex.Add(dex001);
+            ToCatchList.ItemsSource = pokedex;
         }
     }
 }
---
+++ ShareRecipes/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ShareRecipes {
+    public class Pokemon {
+        public string Name {
+            get;
+            set;
+        }
+        public string DexNum {
+            get;
+            set;
+        }
+        public string RoutesSeen {
+            get;
+            set;
+        }
+    }
+}
